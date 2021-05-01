ebf8f88eb56004b960c1817ac277af45b0b5f597
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-05-01 15:56:49 +0530

    cart login register pages

--- server.js
+++ server.js
@@ -8,14 +8,29 @@ const PORT = process.env.PORT || 3300
 //Assets
 app.use(express.static('public'))
 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res, next) => {
 	res.render('home')
 	next()
 })
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res, next) => {
+	res.render('customers/cart')
+	next()
+})
+
+app.get('/login', (req, res, next) => {
+	res.render('auth/login')
+	next()
+})
+
+app.get('/register', (req, res, next) => {
+	res.render('auth/register')
+	next()
+})
 
 app.listen(PORT, () => {
 	console.log(`Listening to port ${PORT}`)
