4ff07689b2093a14f6c4e6bdb81e4794124f577f
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-07-14 05:32:14 +0530

    final changes for  basic app

--- routes/web.js
+++ routes/web.js
@@ -1,19 +1,29 @@
-const authController = require('../app/http/controllers/authController')
-const homeController = require('../app/http/controllers/homeController')
-const cartController = require('../app/http/controllers/customer/cartController')
-const guest = require('../app/http/middlewares/guest')
+const authController = require('../app/http/controllers/authController');
+const homeController = require('../app/http/controllers/homeController');
+const cartController = require('../app/http/controllers/customer/cartController');
+const orderController = require('../app/http/controllers/customer/orderController');
+const adminOrderController = require('../app/http/controllers/admin/orderController');
+const statusController = require('../app/http/controllers/admin/statusController');
+const auth = require('../app/http/middlewares/auth');
+const admin = require('../app/http/middlewares/admin');
+const guest = require('../app/http/middlewares/guest');
 
 function initRoutes(app) {
-	app.get('/', homeController().index)
-	
-	app.get('/login', guest, authController().login)
-    app.post('/login', authController().postLogin)
-    app.get('/register', guest, authController().register)
-    app.post('/register', authController().postRegister)
-    app.post('/logout', authController().logout)
+	app.get('/', homeController().index);
 
-	app.get('/cart', cartController().index)
-	app.post('/update-cart', cartController().update)
+	app.get('/login', guest, authController().login);
+	app.post('/login', authController().postLogin);
+	app.get('/register', guest, authController().register);
+	app.post('/register', authController().postRegister);
+	app.post('/logout', authController().logout);
+
+	app.get('/cart', cartController().index);
+	app.post('/update-cart', cartController().update);
+	app.post('/orders', orderController().store);
+	app.get('/customer/orders', auth, orderController().index);
+	app.get('/customer/orders/:id', auth, orderController().show);
+	app.post('/admin/order/status', admin, statusController().update);
+	app.get('/admin/orders', admin, adminOrderController().index);
 }
 
-module.exports = initRoutes
+module.exports = initRoutes;
--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
-const path = require('path')
-const PORT = process.env.PORT || 3300
-const session = require('express-session')
-const mongoose = require('mongoose')
-const flash = require('express-flash')
-const mongoURI = 'mongodb://localhost/pizza'
-const MongoDbStore = require('connect-mongo')(session)
-const passport = require('passport')
-
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+const PORT = process.env.PORT || 3300;
+const session = require('express-session');
+const mongoose = require('mongoose');
+const flash = require('express-flash');
+const mongoURI = 'mongodb://localhost/pizza';
+const MongoDbStore = require('connect-mongo')(session);
+const passport = require('passport');
+const Emitter = require('events');
 
 mongoose.connect(mongoURI, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
-})
+});
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
 connection
 	.once('open', () => {
-		console.log('Database connected')
+		console.log('Database connected');
 	})
 	.catch((err) => {
-		console.log('Database connection error')
-	})
+		console.log('Database connection error');
+	});
 
 //session store
 let mongoStore = new MongoDbStore({
 	mongooseConnection: connection,
 	collection: 'sessions',
-})
+});
+
+//Emitter
+const eventEmitter = new Emitter();
+app.set('eventEmitter', eventEmitter);
 
 //session
 app.use(
@@ -43,35 +47,52 @@ app.use(
 		store: mongoStore,
 		saveUninitialized: false,
 		cookie: { maxAge: 1000 * 60 * 60 * 24 },
-	}),
-)
+	})
+);
 
-const passportInit = require('./app/config/passport')
-passportInit(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-app.use(flash())
+app.use(flash());
 
 //Assets
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
+app.use(express.json());
 
 //Global middleware
 app.use((req, res, next) => {
-	res.locals.session = req.session
-	res.locals.user = req.user
-	next()
-})
+	res.locals.session = req.session;
+	res.locals.user = req.user;
+	next();
+});
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
-require('./routes/web')(app)
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+require('./routes/web')(app);
 
-app.listen(PORT, () => {
-	console.log(`Listening to port ${PORT}`)
-})
+const server = app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+// Socket
+
+const io = require('socket.io')(server);
+//join
+io.on('connection', (socket) => {
+	socket.on('join', (orderId) => {
+		socket.join(orderId);
+	});
+});
+
+eventEmitter.on('orderUpdated', (data) => {
+	io.to(`order_${data.id}`).emit('orderUpdated', data);
+});
+
+eventEmitter.on('orderPlaced', (data) => {
+	io.to('adminRoom').emit('orderPlaced', data);
+});
